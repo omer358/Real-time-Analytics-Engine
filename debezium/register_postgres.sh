07ed1227944b90141c30030e7583970a515a9fcd
omer358 <omermaki358@gmail.com>
2025-12-22 13:52:16 +0300

    use env vars for docker compose

--- debezium/register_postgres.sh
+++ debezium/register_postgres.sh
@@ -1,21 +1,24 @@
+#!/bin/bash
+set -e
+
 curl -X POST http://debezium:8083/connectors \
   -H "Content-Type: application/json" \
-  -d '{
-    "name": "rta-postgres-cdc",
-    "config": {
-      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-      "database.hostname": "postgres",
-      "database.port": "5432",
-      "database.user": "postgres",
-      "database.password": "postgres",
-      "database.dbname": "rta_ecommerce",
-      "topic.prefix": "rta",
-      "plugin.name": "pgoutput",
-      "decimal.handling.mode": "double",
-      "slot.name": "rta_debezium_slot",
-      "publication.autocreate.mode": "filtered",
-      "table.include.list": "public.product",
-      "tombstones.on.delete": "false",
-      "include.schema.changes": false
+  -d "{
+    \"name\": \"rta-postgres-cdc\",
+    \"config\": {
+      \"connector.class\": \"io.debezium.connector.postgresql.PostgresConnector\",
+      \"database.hostname\": \"${RTA_DB_HOST}\",
+      \"database.port\": \"${RTA_DB_PORT}\",
+      \"database.user\": \"${RTA_DB_USER}\",
+      \"database.password\": \"${RTA_DB_PASSWORD}\",
+      \"database.dbname\": \"${RTA_DB_NAME}\",
+      \"topic.prefix\": \"rta\",
+      \"plugin.name\": \"pgoutput\",
+      \"decimal.handling.mode\": \"double\",
+      \"slot.name\": \"rta_debezium_slot\",
+      \"publication.autocreate.mode\": \"filtered\",
+      \"table.include.list\": \"public.product\",
+      \"tombstones.on.delete\": \"false\",
+      \"include.schema.changes\": false
     }
-  }'
+  }"
